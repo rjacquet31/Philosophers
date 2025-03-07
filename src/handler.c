9970cde2f00440253422f2089d1b0d37b778cc85
deluxeee31 <rafael.jacquet.31@gmail.com>
2025-03-07 15:32:44 +0100

    norm and fix

--- src/handler.c
+++ src/handler.c
@@ -30,18 +30,18 @@ int	wrong_format(void)
 	return (ERROR);
 }
 
-int digits(const char *str)
+int	digits(const char *str)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (str[i] != '\0')
-    {
-        if (str[i] < '0' || str[i] > '9')
-            return (0);
-        i++;
-    }
-    return (1);
+	i = 0;
+	while (str[i] != '\0')
+	{
+		if (str[i] < '0' || str[i] > '9')
+			return (0);
+		i++;
+	}
+	return (1);
 }
 
 int	check_input(int ac, char **av)
